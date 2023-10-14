d2159c6d2f2a356cc61074f8feab3e6bf2e60c53
Michael Olubode <olubodem@gmail.com>
2023-10-14 12:14:18 -0500

    added conversational retrieval to chainlit

--- src/app.py
+++ src/app.py
@@ -2,19 +2,24 @@ import chainlit as cl
 from dotenv import load_dotenv
 from langchain.document_loaders import ConfluenceLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-# import chromadb
 from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.chains import ConversationalRetrievalChain
+
 
 # built-in modules
 import os
+
 load_dotenv()
 
 
-def load_confluence_documents(url, username, api_token, limit=-1):
+def load_confluence_documents(url, username, CONFLUENCE_API_TOKEN, limit=-1):
     # get documents from confluence
     loader = ConfluenceLoader(
-        url=url, username=username, api_key=api_token, 
+        url=url,
+        username=username,
+        api_key=CONFLUENCE_API_TOKEN,
     )
     if limit != -1:
         documents = loader.load(space_key=space_key, limit=limit)
@@ -29,48 +34,63 @@ def split_document(documents, chunk_size, overlap):
         chunk_overlap=overlap,
         length_function=len,
         add_start_index=True,
+        separators=[" ", "\n", ".", ","],
     )
     texts = text_splitter.split_documents(documents)
     return texts
 
 
-def get_retriever(embeddings, texts, persist_directory):
+def get_vectorstore(embeddings, texts, persist_directory):
+    """
+    embeddings: text embedding model instance
+    texts: list of texts to embed
+    persist_directory: directory to persist vectors
+    """
     # Check if the folder exists in the current working directory
     if os.path.exists(persist_directory) and os.path.isdir(persist_directory):
-        print(f"The folder '{persist_directory}' exists in the current working directory.")
+        print(
+            f"The folder '{persist_directory}' exists in the current working directory. Hence embeddings will be loaded from disk"
+        )
         cdb = Chroma(embedding_function=embeddings, persist_directory=persist_directory)
     else:
-        print(f"The folder '{persist_directory}' does not exist in the current working directory.")
-        cdb = Chroma.from_documents(texts, embedding=embeddings, persist_directory=persist_directory)
-    
-    retriever = cdb.as_retriever(
-        search_kwargs={"k": 5}
-    )
-    return retriever
+        print(
+            f"The folder '{persist_directory}' does not exist in the current working directory. Hence embeddings will be created afresh"
+        )
+        cdb = Chroma.from_documents(
+            texts, embedding=embeddings, persist_directory=persist_directory
+        )
+    return cdb
 
 
 confluence_url = "https://mikesofts.atlassian.net/wiki"
-email = os.environ['EMAIL']
-api_token = os.environ['API_TOKEN']
+email = os.environ["EMAIL"]
+CONFLUENCE_API_TOKEN = os.environ["CONFLUENCE_API_TOKEN"]
 space_key = "~614914d4071141006ab46038"
 
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+embeddings = GooglePalmEmbeddings(google_api_key=os.environ["GOOGLE_PALM_API_KEY"])
+googlepalm_llm = GooglePalm(
+    google_api_key=os.environ["GOOGLE_PALM_API_KEY"], temperature=0.1
+)
+
 persist_directory = "chroma_db"
 
-documents = load_confluence_documents(confluence_url, email, api_token, limit=-1)
-texts = split_document(documents, chunk_size=100, overlap=10)
-retriever = get_retriever(embeddings, texts, persist_directory)
+documents = load_confluence_documents(
+    confluence_url, email, CONFLUENCE_API_TOKEN, limit=-1
+)
+texts = split_document(documents, chunk_size=1000, overlap=100)
+vectorstore = get_vectorstore(embeddings, texts, persist_directory)
 
-# implement conversionalretrieval chain
-# google PALM, MakerSuite for LLM
+qa_chain = ConversationalRetrievalChain.from_llm(
+    googlepalm_llm,
+    retriever=vectorstore.as_retriever(),
+    verbose=False,
+    return_source_documents=True,
+)
+chat_history = []
+
+
 @cl.on_message
-async def main(question: str):
-    documents_results = retriever.get_relevant_documents(question)
-    text_elements = [cl.Text(content=doc.metadata['source'], name=doc.metadata['title']) for doc in documents_results]
-
-    source_names = [text_el.name for text_el in text_elements]
-    results = "\n\n".join([f"{doc.page_content}: {source}" for doc, source in zip(documents_results, source_names)])
-
-    await cl.Message(
-        content=results, elements=text_elements
-    ).send()
+async def main(query: str):
+    result = qa_chain({"question": query, "chat_history": chat_history})
+    await cl.Message(content=result["answer"]).send()
+    chat_history.append((query, result["answer"]))
